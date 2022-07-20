52a1804fcf66a2791ef4827d6dab40b7ad253983
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-07-20 13:44:01 +0200

    show response time and status code

--- src/view/PageOverlay.vue
+++ src/view/PageOverlay.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
-    <h4 class="title">Header Explorer</h4>
+    <div class="row">
+      <div class="col-6"><h4 class="title">KEEN-EYE</h4></div>
+      <div class="col-6">
+        <CopyHeadersButtonVue :headers="responseHeaders"></CopyHeadersButtonVue>
+      </div>
+    </div>
+
     Settings loaded from <b>{{ level }}</b>
     <br />
-    settings: {{ settings }}
-    <br />
-    <CopyHeadersButtonVue :headers="responseHeaders"></CopyHeadersButtonVue>
+    <!--   settings: {{ settings }}
+    <br /> -->
+    <div class="row">
+      <div class="col-6">
+        Status code: <b>{{ requestInfo?.response?.statusCode ?? "loading" }}</b>
+      </div>
+      <div class="col-6">
+        Response took: <b>{{ time ? time + "ms" : "loading" }}</b>
+      </div>
+    </div>
+    Try to refresh page if it is taking too long to load.Turn of in popup
     <HeaderRendererVue :headers="responseHeaders"> </HeaderRendererVue>
   </div>
 </template>
@@ -15,6 +29,7 @@ import HeaderRendererVue from "@/components/HeaderRenderer.vue";
 import { getRequestInfo } from "@/helpers/scriptsComunicationHelper";
 import CopyHeadersButtonVue from "@/components/CopyHeadersButton.vue";
 import { matchWithStairs } from "@/helpers/stringHelpers";
+import { toRaw } from "@vue/reactivity";
 export default {
   components: {
     HeaderRendererVue,
@@ -37,9 +52,7 @@ export default {
       )
         return [];
 
-      console.log(matchWithStairs("test", "test"));
-      console.log(matchWithStairs("test11", "test*"));
-      console.log(matchWithStairs("11test", "*test"));
+      console.log(toRaw(this.requestInfo));
       return this.requestInfo.response.responseHeaders.filter(
         ({ name: headerName }) => {
           return this.settings?.headerRules?.some((allowed) => {
@@ -47,6 +60,20 @@ export default {
           });
         }
       );
+    },
+    time() {
+      console.log(this.requestInfo);
+      console.log(this.requestInfo?.response?.timeStamp);
+      console.log(this.requestInfo?.request?.timeStamp);
+      if (
+        !this.requestInfo ||
+        !this.requestInfo?.response?.timeStamp ||
+        !this.requestInfo?.request?.timeStamp
+      )
+        return undefined;
+      return (
+        this.requestInfo.response.timeStamp - this.requestInfo.request.timeStamp
+      ).toFixed(2);
     },
   },
   methods: {
