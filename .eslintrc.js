0a78c5b8c5d51d0278127f3df57c64cb167c1864
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-08-13 12:48:21 +0200

    attemp at adding prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,35 @@
+/* eslint-disable no-undef */
 module.exports = {
+	env: {
+		node: true,
+	},
 	extends: [
 		// add more generic rulesets here, such as:
-		// 'eslint:recommended',
-		"plugin:vue/vue3-recommended"
+		"eslint:recommended",
+		"plugin:vue/vue3-recommended",
 		// 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
 	],
 	rules: {
-		// override/add rules settings here, such as:
-		// 'vue/no-unused-vars': 'error'
-		"vue/multi-word-component-names": "off",
-		"indent": ["error", "tab", {SwitchCase: 1}],
-		"vue/html-indent": ["error", "tab"]
-	}
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		quotes: ["warn", "double"],
+		indent: [
+			"warn",
+			"tab",
+			{
+				SwitchCase: 1,
+			},
+		],
+		"no-tabs": "off",
+		"space-before-function-paren": "off",
+		"object-curly-spacing": ["error", "never"],
+		curly: "off",
+		"prefer-const": "warn",
+		"no-mixed-operators": "off",
+		"no-unused-vars": "warn",
+		"no-eval": "off",
+		eqeqeq: "off",
+		"operator-linebreak": "off",
+		"vue/no-unused-components": "warn",
+	},
 }
