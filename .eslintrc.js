e085e2e4c02c06a3ff3e90f32a9a13faed5c42da
Filip Jakab <jakabfilip.fj@gmail.com>
2022-08-23 16:12:57 +0200

    Fixed eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
 		"vue/multi-word-component-names": "off",
 		indent: ["error", "tab", {SwitchCase: 1}],
 		"vue/html-indent": "off",
-		"vue/mustache-interpolation-spacing": ["error", 0],
+		"vue/mustache-interpolation-spacing": ["error", "never"],
 		"vue/require-default-prop": "off",
 		"vue/html-self-closing": [
 			"error",
